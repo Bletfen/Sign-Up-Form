10cb3e65416c5f14bc3584cde861139a305881dc
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-16 22:11:51 +0400

    favicon

--- src/components/maintext/MainText.jsx
+++ src/components/maintext/MainText.jsx
@@ -1,3 +1,12 @@
 export default function MainText() {
-  return <div>MainText</div>;
+  return (
+    <div>
+      <h1>Learn to code by watching others</h1>
+      <p>
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.{" "}
+      </p>
+    </div>
+  );
 }
