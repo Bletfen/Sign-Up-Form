5c3bad557a7af3048b9ebd44b8053a6f9c28c8e7
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-16 22:38:47 +0400

    test

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,6 +1,6 @@
 export default function SignUp() {
   return (
-    <div class="py-[1.8rem] px-[6.6rem]">
+    <div class="">
       <p>
         Try it free 7 days<span> then $20/mo. thereafter</span>
       </p>
