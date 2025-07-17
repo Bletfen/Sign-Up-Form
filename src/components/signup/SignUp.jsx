53e009a3e15dd744784d0ab343945201810e4066
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-17 19:04:21 +0400

    mobile first

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,22 +1,52 @@
+import Trial from "./trial/Trial";
 export default function SignUp() {
+  const signUpForm = ["First Name", "Last Name", "Email Address", "Password"];
   return (
-    <div
-      className="p-px 
-      rounded-[1rem] 
-      bg-[#5e54a4] 
-      mt-[6.4rem]"
-    >
-      <p
-        className="text-center 
-        text-[#fff] 
-        text-[1.5rem] 
-        font-medium 
-        leading-[2.6rem] 
-        tracking-[0.268px]"
+    <div className="flex flex-col gap-[2.4rem]">
+      <Trial />
+      <form
+        action="/signup"
+        className="flex flex-col gap-[1.6rem] p-[2.4rem] bg-[#fff]
+        rounded-[1rem] shadow-[0_0.8rem_0_0_rgba(0,0,0,0.15)]"
       >
-        Try it free 7 days
-        <span className="font-normal"> then $20/mo. thereafter</span>
-      </p>
+        {signUpForm.map((form) => {
+          return (
+            <div
+              key={form}
+              className="py-[1.5rem] border-solid border border-[#dedede]
+              rounded-[0.5rem] pl-[1.9rem]"
+            >
+              <label htmlFor={form}></label>
+              <input
+                type="text"
+                id={form}
+                name={form}
+                placeholder={form}
+                required
+                className="text-[1.4rem] font-[600] outline-none
+                leading-[2.6rem]
+                tracking-[0.25px]"
+              />
+            </div>
+          );
+        })}
+        <div>
+          <button
+            className="py-[1.5rem] px-[4.4rem] text-center
+            bg-[#38cc8B]
+            shadow-[0_-0.4rem_0_0_rgba(0,0,0,0.09)_inset]
+            rounded-[0.5rem]
+            text-[1.5rem] text-[#fff]
+            font-[600] leading-[2.6rem] tracking-[0.1rem] cursor-[pointer]"
+          >
+            CLAIM YOUR FREE TRIAL
+          </button>
+          <p>
+            By clicking the button, you are agreeing to our
+            <span>Terms and Services</span>
+          </p>
+        </div>
+      </form>
     </div>
   );
 }
--- src/components/signup/trial/Trial.jsx
+++ src/components/signup/trial/Trial.jsx
@@ -1,8 +1,25 @@
 import SignUp from "../SignUp";
 export default function Trial() {
   return (
-    <div>
-      <SignUp />
+    <div
+      className=" 
+      py-[1.8rem]
+      px-[6.6rem]
+      rounded-[1rem] 
+      bg-[#5e54a4] 
+      shadow-[0_0.8rem_0_0_rgba(0,0,0,0.15)]"
+    >
+      <p
+        className="text-center 
+        text-[#fff] 
+        text-[1.5rem] 
+        font-medium 
+        leading-[2.6rem] 
+        tracking-[0.268px]"
+      >
+        Try it free 7 days
+        <span className="font-normal"> then $20/mo. thereafter</span>
+      </p>
     </div>
   );
 }
