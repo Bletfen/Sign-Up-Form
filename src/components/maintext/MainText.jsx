750f4429643af9f3772415da9fa4be9e65c29204
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-17 20:02:53 +0400

    maintext-desktop

--- src/components/maintext/MainText.jsx
+++ src/components/maintext/MainText.jsx
@@ -1,10 +1,11 @@
 export default function MainText() {
   return (
-    <div className="flex flex-col text-center gap-[1.6rem]">
+    <div className="flex flex-col text-center gap-[1.6rem] max-w-[54rem] xl:text-start">
       <h1
         className="text-white text-[2.8rem] 
         font-bold leading-[3.6rem]
-        tracking-[-0.292px]"
+        tracking-[-0.292px]
+        xl:text-[5rem] xl:leading-[5.5rem] xl:tracking-[-0.521px]"
       >
         Learn to code by watching others
       </h1>
