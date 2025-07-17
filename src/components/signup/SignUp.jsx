ae4f50095569cec027ed5b105438b235725f1d11
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-17 17:41:55 +0400

    day two

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,8 +1,22 @@
 export default function SignUp() {
   return (
-    <div class="">
-      <p>
-        Try it free 7 days<span> then $20/mo. thereafter</span>
+    <div
+      class="py-[1.8rem] 
+      px-[6.6rem] 
+      rounded-[1rem] 
+      bg-[#5e54a4] 
+      mt-[6.4rem]"
+    >
+      <p
+        class="text-center 
+        text-[#fff] 
+        text-[1.5rem] 
+        font-medium 
+        leading-[2.6rem] 
+        tracking-tighter-[0.268px]"
+      >
+        Try it free 7 days
+        <span className="font-normal"> then $20/mo. thereafter</span>
       </p>
     </div>
   );
