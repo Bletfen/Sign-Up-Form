28bd5ace3990bbc76f143b4aa41ac9bead752878
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-17 19:50:14 +0400

    logic

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,5 +1,28 @@
 import Trial from "./trial/Trial";
+import { useState } from "react";
 export default function SignUp() {
+  const [errors, setErrors] = useState({});
+  const [formValues, setFormValues] = useState({
+    "First Name": "",
+    "Last Name": "",
+    "Email Address": "",
+    Password: "",
+  });
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newErrors = {};
+    for (const key in formValues) {
+      if (!formValues[key].trim()) {
+        newErrors[key] = `${key} can not be empty`;
+      }
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const email = formValues["Email Address"];
+    if (email.trim() && !emailRegex.test(email)) {
+      newErrors["Email Address"] = `Looks like this is not an email`;
+    }
+    setErrors(newErrors);
+  }
   const signUpForm = ["First Name", "Last Name", "Email Address", "Password"];
   return (
     <div className="flex flex-col gap-[2.4rem]">
@@ -8,25 +31,68 @@ export default function SignUp() {
         action="/signup"
         className="flex flex-col gap-[1.6rem] p-[2.4rem] bg-[#fff]
         rounded-[1rem] shadow-[0_0.8rem_0_0_rgba(0,0,0,0.15)]"
+        onSubmit={handleSubmit}
       >
         {signUpForm.map((form) => {
           return (
-            <div
-              key={form}
-              className="py-[1.5rem] border-solid border border-[#dedede]
-              rounded-[0.5rem] pl-[1.9rem]"
-            >
-              <label htmlFor={form}></label>
-              <input
-                type="text"
-                id={form}
-                name={form}
-                placeholder={form}
-                required
-                className="text-[1.4rem] font-[600] outline-none
+            <div>
+              <div
+                key={form}
+                className="py-[1.5rem] border-solid border border-[#dedede]
+              rounded-[0.5rem] pl-[1.9rem] flex flex-row justify-between pr-[2.7rem]"
+              >
+                <input
+                  type="text"
+                  id={form}
+                  name={form}
+                  placeholder={form}
+                  className="text-[1.4rem] font-[600] outline-none
                 leading-[2.6rem]
                 tracking-[0.25px]"
-              />
+                  onChange={(e) =>
+                    setFormValues({
+                      ...formValues,
+                      [e.target.name]: e.target.value,
+                    })
+                  }
+                />
+                {errors[form] ? (
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="12" cy="12" r="12" fill="#FF7979" />
+                    <rect
+                      x="11"
+                      y="6"
+                      width="2"
+                      height="9"
+                      rx="1"
+                      fill="white"
+                    />
+                    <rect
+                      x="11"
+                      y="17"
+                      width="2"
+                      height="2"
+                      rx="1"
+                      fill="white"
+                    />
+                  </svg>
+                ) : (
+                  ""
+                )}
+              </div>
+              {errors[form] ? (
+                <p className="text-right font-[1.1rem] text-[#ff7979] italic font-[500] mt-[0.6rem]">
+                  {errors[form]}
+                </p>
+              ) : (
+                ""
+              )}
             </div>
           );
         })}
